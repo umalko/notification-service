778d5a99110f722bec5366896b4b8ba96f7e8d9b
Vladimir Malko <mavs.vl@gmail.com>
2018-11-30 18:18:26 +0300

    added postgress and enabled eureka

--- src/main/java/com/mavs/notificationservice/NotificationServiceApplication.java
+++ src/main/java/com/mavs/notificationservice/NotificationServiceApplication.java
@@ -2,11 +2,13 @@ package com.mavs.notificationservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class NotificationServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(NotificationServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(NotificationServiceApplication.class, args);
+    }
 }
